843c0fb9228e687051d9d976c462bb4b98cf2d2d
Vilmantas Petrusevičius <15175886+vilmantas@users.noreply.github.com>
2022-08-14 16:18:06 +0300

    Health Pill remove healing on pickup. Add prefabs for UI.

--- Assets/_SampleGames/Survivr/Scripts/Behaviours/HealthPillController.cs
+++ Assets/_SampleGames/Survivr/Scripts/Behaviours/HealthPillController.cs
@@ -1,4 +1,3 @@
-using Features.Health;
 using Features.Inventory;
 using Features.Items;
 using UnityEngine;
@@ -7,38 +6,26 @@ namespace _SampleGames.Survivr
 {
     public class HealthPillController : MonoBehaviour
     {
-        public int HealValue = 1;
-
+        public Item_SO ItemMetadata;
         private bool m_IsExpended;
 
-        public Item_SO ItemMetadata;
-        
         private void OnTriggerEnter(Collider other)
         {
             if (m_IsExpended) return;
-            
+
             var character = other.transform.root.GetComponent<CharacterController>();
 
             if (character == null) return;
-
-            var health = character.GetComponentInChildren<HealthController>();
-
-            if (health == null) return;
-            
-            m_IsExpended = true;
-            
-            health.Heal(HealValue);
 
             var inventory = character.GetComponentInChildren<InventoryController>();
 
             if (inventory)
             {
-                
                 var request = ChangeRequestFactory.Add(ItemMetadata.MakeInstance(), 1);
 
                 inventory.HandleRequest(request);
             }
-            
+
             Destroy(gameObject);
         }
     }
