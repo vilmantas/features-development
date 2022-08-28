6218e086b24bc794bd0611c355f2d780f91fcf91
Vilmantas Petrusevičius <15175886+vilmantas@users.noreply.github.com>
2022-08-28 19:14:41 +0300

    Structure changes. Fix character debug scene.

---
+++ Assets/DebugScripts/Buffs/LifterImplementation.cs
@@ -0,0 +1,42 @@
+using Features.Buffs;
+using UnityEngine;
+
+namespace Features.Character.Buffs
+{
+    public static class LifterImplementation
+    {
+        [RuntimeInitializeOnLoadMethod]
+        private static void Register()
+        {
+            BuffImplementation implementation = new("Lifter", OnReceive, OnRemove, null, null);
+            BuffImplementationRegistry.Implementations.TryAdd(implementation.Name, implementation);
+        }
+
+        private static void OnReceive(BuffActivationPayload payload)
+        {
+            var liftAmount = Vector3.up * 5;
+
+            payload.Target.transform.Translate(liftAmount);
+
+            payload.Buff.State = new LifterState("Lifted", liftAmount);
+        }
+
+        private static void OnRemove(BuffActivationPayload payload)
+        {
+            payload.Target.transform.Translate(-((LifterState) payload.Buff.State).LiftAmount);
+        }
+
+        private class LifterState : IBuffState
+        {
+            public LifterState(string description, Vector3 liftAmount)
+            {
+                Description = description;
+                LiftAmount = liftAmount;
+            }
+
+            public Vector3 LiftAmount { get; }
+
+            public string Description { get; }
+        }
+    }
+}
