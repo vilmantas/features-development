b66a2f95d64ecda2b66b23fb5dfc6398642e676b
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2023-01-09 17:04:15 +0200

    Merge

--- Assets/Managers/IntroController.cs
+++ Assets/Managers/IntroController.cs
@@ -1,12 +1,25 @@
+using System;
 using UnityEngine;
 
 namespace Managers
 {
     public class IntroController : MonoBehaviour
     {
+        public bool SkipIntro;
+        
+        private void Start()
+        {
+#if UNITY_EDITOR
+            if (!SkipIntro) return;
+            
+            GameManager.Instance.StartGame();
+#endif
+        }
+
         public void StartGame()
         {
-            GameManager.Instance.StartGame();
+
+            
         }
     }
 }
