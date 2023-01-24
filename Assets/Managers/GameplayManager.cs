d33f4b6756d39945fc5021193884daeb303d9882
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2023-01-24 09:47:35 +0200

    Refactor: Move Player movement triggers to gameplay manager from user mouse input controller

--- Assets/Managers/GameplayManager.cs
+++ Assets/Managers/GameplayManager.cs
@@ -2,6 +2,9 @@ using System;
 using Features.Camera;
 using Features.Character;
 using Features.CharacterModel;
+using Features.Movement;
+using Features.Targeting;
+using Integrations.Actions;
 using UnityEngine;
 
 namespace Managers
@@ -10,6 +13,12 @@ namespace Managers
     {
         [HideInInspector] public Player Player;
 
+        private UserMouseInputController m_MouseInputController;
+        
+        public bool DisableInput;
+
+        public bool DisableMovement;
+        
         protected override void DoSetup()
         {
             Player = GameObject.Find("Player").GetComponent<Player>();
@@ -22,6 +31,52 @@ namespace Managers
             var characterModel = Player.GetComponentInChildren<CharacterModelController>();
             
             cameraManager.ChangeTarget(characterModel.CameraFollow, characterModel.CameraTarget);
+
+            m_MouseInputController = UserMouseInputController.Instance;
+            
+            m_MouseInputController.OnGameGroundClicked += OnGameGroundClicked;
+            
+            m_MouseInputController.OnCharacterLocationClicked += OnCharacterLocationClicked;
+            
+            LocationProvider.OnOverlayActivated += OnOverlayActivated;
+            
+            LocationProvider.OnOverlayDisabled += OnOverlayDisabled;
+        }
+
+        private void OnOverlayDisabled()
+        {
+            DisableInput = false;
+            DisableMovement = false;
+        }
+
+        private void OnOverlayActivated(OverlayInfo info)
+        {
+            DisableInput = true;
+
+            if (info.BlockMovementActions)
+            {
+                DisableMovement = true;
+            }
+        }
+        
+        private void OnGameGroundClicked(Vector3 point)
+        {
+            if (DisableInput && DisableMovement) return;
+            
+            var movePayload =
+                Move.MakePayload(Player.gameObject, new MoveActionData(point));
+
+            Player.m_ActionsController.DoAction(movePayload);
+        }
+        
+        private void OnCharacterLocationClicked(Vector3 characterLocation)
+        {
+            if (DisableInput) return;
+            
+            var movePayload =
+                Move.MakePayload(Player.gameObject, new MoveActionData(characterLocation));
+
+            Player.m_ActionsController.DoAction(movePayload);
         }
     }
 }
--- Assets/Managers/UserMouseInputController.cs
+++ Assets/Managers/UserMouseInputController.cs
@@ -11,92 +11,36 @@ namespace Managers
 {
     public class UserMouseInputController : SingletonManager<UserMouseInputController>
     {
-        private Player m_Player;
-
-        private GameObject m_PlayerRoot;
-        
         public LayerMask GroundAndPlayerMask;
 
-        public LayerMask GroundMask;
-        
-        public bool DisableInput = false;
+        public Action<Vector3> OnGameGroundClicked;
 
-        public bool DisableMovement = false;
+        public Action<Vector3> OnCharacterLocationClicked;
 
         private void Start()
         {
-            m_Player = GameplayManager.Instance.Player;
-
-            m_PlayerRoot = m_Player.transform.root.gameObject;
-
             GroundAndPlayerMask = LayerMask.GetMask("Ground", "PlayerHitbox");
-            
-            GroundMask = LayerMask.GetMask("Ground");
-            
-            LocationProvider.OnOverlayActivated += OnOverlayActivated;
-            
-            LocationProvider.OnOverlayDisabled += OnOverlayDisabled;
-        }
-        
-        private void OnOverlayDisabled()
-        {
-            DisableInput = false;
-            DisableMovement = false;
-        }
-
-        private void OnOverlayActivated(OverlayInfo info)
-        {
-            DisableInput = true;
-
-            if (info.BlockMovementActions)
-            {
-                DisableMovement = true;
-            }
         }
 
         private void Update()
         {
-            if (DisableInput)
+            if (!Input.GetMouseButtonUp(0)) return;
+            
+            if (EventSystem.current != null &&
+                EventSystem.current.IsPointerOverGameObject()) return;
+            
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, 100f, GroundAndPlayerMask)) return;
+
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("PlayerHitbox"))
             {
-                if (DisableMovement) return;
-                
-                if (Input.GetMouseButtonUp(0))
-                {
-                    if (EventSystem.current != null &&
-                        EventSystem.current.IsPointerOverGameObject()) return;
-                
-                    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                OnCharacterLocationClicked?.Invoke(hit.point);
 
-                    if (!Physics.Raycast(ray, out RaycastHit hit, 100f, GroundAndPlayerMask)) return;
-                    
-                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("PlayerHitbox"))
-                        return;
-                        
-                    var movePayload =
-                        Move.MakePayload(m_PlayerRoot, new MoveActionData(hit.point));
-
-                    m_Player.m_ActionsController.DoAction(movePayload);
-                }
-                
                 return;
             }
-            
-            if (Input.GetMouseButtonUp(0))
-            {
-                if (EventSystem.current != null &&
-                    EventSystem.current.IsPointerOverGameObject()) return;
 
-
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(ray, out RaycastHit hit, 100f, GroundMask))
-                {
-                    var movePayload =
-                        Move.MakePayload(m_Player.transform.root.gameObject, new MoveActionData(hit.point));
-
-                    m_Player.m_ActionsController.DoAction(movePayload);
-                }
-            }
+            OnGameGroundClicked?.Invoke(hit.point);
         }
     }
 }
