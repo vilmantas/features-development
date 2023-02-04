5551652193170166bfa8e1ba6e8acd317e8d0013
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2023-02-04 13:32:23 +0200

    Minor: Add healing particles.

--- Assets/Integrations/Skills/Heal.cs
+++ Assets/Integrations/Skills/Heal.cs
@@ -1,4 +1,6 @@
 using Features.Actions;
+using Features.CharacterModel;
+using Features.ParticlePlayer;
 using Features.Skills;
 using Integrations.Actions;
 using UnityEngine;
@@ -8,12 +10,20 @@ namespace Integrations.Skills
     public static class HealSkill
     {
         public static readonly int HEAL_DEFAULT_VALUE = 10;
+
+        private static ParticleSystem m_HealParticles;
         
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Register()
         {
             SkillImplementation implementation = new(OnActivation);
             SkillImplementationRegistry.Register(nameof(HealSkill), implementation);
+
+            var particles = Resources.Load<ParticleSystem>("Particles/Heal");
+
+            if (!particles) return;
+
+            m_HealParticles = particles;
         }
         
         private static SkillActivationResult OnActivation(SkillActivationContext context)
@@ -25,14 +35,23 @@ namespace Integrations.Skills
                 healVal = (int)data.NumericValue;
             }
             
-            var defendAction = Heal.MakePayload(context.Source, context.TargetObject, healVal);
+            var healAction = Heal.MakePayload(context.Source, context.TargetObject, healVal);
 
             var result = context.Source.GetComponentInChildren<ActionsController>()
-                .DoAction(defendAction);
+                .DoAction(healAction);
 
-            return new SkillActivationResult(result == ActionActivationResult.NoResultActivation ||
-                                             result.IsSuccessful.HasValue &&
-                                             result.IsSuccessful.Value);
+            var success = result == ActionActivationResult.NoResultActivation ||
+                          result.IsSuccessful.HasValue &&
+                          result.IsSuccessful.Value;
+
+            if (!success) return new SkillActivationResult(false);
+            
+            var x = context.TargetObject.GetComponentInChildren<CharacterModelController>();
+                
+            ParticlePlayerController.Instance.PlayParticles(m_HealParticles,
+                x.HeadLocation.position);
+
+            return new SkillActivationResult(true);
         }
     }
 }
